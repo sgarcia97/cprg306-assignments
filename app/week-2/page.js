024af9ada2ed74cc5437f0dcdeb9fd40044ca4ae
sgarcia97 <83793406+sgarcia97@users.noreply.github.com>
2025-01-21 15:15:14 -0700

    Changes and updates made for lab 1

---
+++ app/week-2/page.js
@@ -0,0 +1,10 @@
+import StudentInfo from './student-info'
+
+export default function Page() {
+    return (
+        <main>
+            <h2 className="text-xl font-medium">Shopping List</h2>
+            <StudentInfo/>
+        </main>
+    );
+}
---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function StudentInfo(){
+    return(
+        <main>
+            <p>Stefan Garcia</p>
+            <Link href="https://github.com/sgarcia97">My Github Repository</Link>
+        </main>
+    );
+}
