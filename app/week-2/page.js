78fd4e25c91068c7d0a719c61ecbac363f7e98f6
sgarcia97 <83793406+sgarcia97@users.noreply.github.com>
2025-01-21 16:05:23 -0700

    Updates to styling

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -2,7 +2,7 @@ import StudentInfo from './student-info'
 
 export default function Page() {
     return (
-        <main>
+        <main className="main">
             <h2 className="text-xl font-medium">Shopping List</h2>
             <StudentInfo/>
         </main>
