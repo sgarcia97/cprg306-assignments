196631a243e80151697f1d0b4782324914651972
sgarcia97 <83793406+sgarcia97@users.noreply.github.com>
2025-03-05 22:50:40 -0700

    week-7 styling updates

--- app/week-7/page.js
+++ app/week-7/page.js
@@ -1,10 +1,13 @@
 import React from "react"
 import ItemList from "./item-list"
+import NewItem from "./new-item"
 
 export default function Page(){
     return(
         <main className="main">
             <h2 className="text-3xl font-medium">Shopping List</h2>
+            <NewItem/>
+            <p></p>
             <ItemList/>
         </main>
     )
