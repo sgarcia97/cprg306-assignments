24728afc0bc73de47889866a0911aa98b16ccd60
sgarcia97 <83793406+sgarcia97@users.noreply.github.com>
2025-03-22 23:18:44 -0600

    Updates to Assignment 10

---
+++ app/week-10/_services/shopping-list-service.js
@@ -0,0 +1,18 @@
+import { db } from "../_utils/firebase";
+import { collection, getDocs, addDoc, query } from "firebase/firestore";
+
+export const getItems = async (userId) => {
+
+
+}
+
+export const addItems = async (userId, item) => {
+
+
+}
+
+export const deleteItem = async (userId, item) => {
+
+
+}
+
---
+++ app/week-10/shopping-list/page.js
@@ -0,0 +1,76 @@
+"use client"
+import { useUserAuth } from "../_utils/auth-context";
+import { redirect } from 'next/navigation';
+import React from "react"
+import { getItems, addItem } from "../_services/shopping-list-service"
+import ItemList from "./item-list"
+import NewItem from "./new-item"
+import { MealIdeas } from "./meal-ideas"
+import { useState, useEffect } from "react"
+import Link from "next/link";
+
+
+export default function Page(){
+    const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
+    const [items, setItems] = useState(itemsData)
+    const [selectedItemname, setSelectedItemname] = useState("bananas")
+
+   
+
+    useEffect(()=>{
+        const loadItems = async () => {
+            const it = getItems(user.uid)
+            setItems(it)
+        }
+        loadItems()
+    },[user.uid])
+
+    const handleAddItem = (obj) => { 
+        addItem(user.uid, obj)
+       // const newitems = [...items]
+       // newitems.push(obj)
+        setItems(items)
+    }
+
+    const logOut = async () => {
+        firebaseSignOut()
+    }
+
+    const handleItemSelect = (item) => {
+        const wordsToRemove = ["breasts", "L", "g", "sauce", "kg", "pack", "dozen"];
+        const text = item;
+        const wordRegex = new RegExp(`\\b(${wordsToRemove.join("|")})\\b|,`, "gi");
+        const emojiRegex = /([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF]|[\d])/g;
+
+        const result = text
+        .replace(wordRegex, "")      // Remove words
+        .replace(emojiRegex, "")     // Remove emojis
+        .replace(/\s+/g, " ")        // Collapse spaces
+        .trim();
+        setSelectedItemname(result)
+    }
+
+    return(
+        <>
+        {
+            user ?
+        (
+        <main className="main split">
+            <div className="main-sub">
+            <h2 className="text-3xl font-medium">Shopping List <button onClick={logOut} className="button1" style={{fontSize:14}}>Logout</button></h2>
+            <NewItem onAddItem={handleAddItem}/>
+            <p></p>
+            <ItemList items={items} onItemSelect={()=>handleItemSelect}/>
+            </div>
+            <MealIdeas ingredient={selectedItemname}/>
+        </main>
+        )
+        : 
+        redirect('/week-9')
+        (
+            <div style={{textAlign:"center", padding:10}}>You are not authorized to view this page! <Link href="http://localhost:3001">Go to home page</Link></div>
+        )
+    }
+        </>
+    )
+}
